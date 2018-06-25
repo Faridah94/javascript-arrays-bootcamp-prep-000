684fac8de7d083110049661bf2e96a12be130910
Farida <faridah.absi@gmail.com>
2018-06-25 08:27:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat"," skittles"]
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
+ 
+ 
